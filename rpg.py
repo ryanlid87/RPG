cd6aeeda543f3d5ec83f15af97d21c3f17290a96
ryanlid87 <rlidster1@gmail.com>
2016-01-07 13:34:09 -0600

    Create rpg.py
    Basic start

---
+++ rpg.py
@@ -0,0 +1,38 @@
+from random import randint
+
+class Character(object):
+    def __init__(self,name,hp,atk,defence,exp,coin):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.defence = defence
+        self.exp = exp
+        self.coin = coin
+        
+    def attack(self,monster):
+        if self.atk < monster.defence:
+            return '%s was not hurt their defence is too high' %(monster.name)
+        else:
+            monster.hp = monster.hp -(self.atk - monster.defence)
+        if monster.hp < 1:
+            self.exp = self.exp + monster.exp
+            self.coin = self.coin + monster.coin
+            return '%s died and gave %s exp and %s coins.' %(monster.name,monster.exp,monster.coin)
+        return '%s was attacked for %s HP = %s' %(monster.name,(self.atk-monster.defence),monster.hp)
+
+    def run(self,monster):
+        chance = randint(1,2)
+        if chance%2 == 0:
+            return '%s ran sucessfully.' %(self.name)
+        else:
+            return '%s was too fast %s could not get away.' %(monster.name,self.name)
+        
+class Monster(Character):
+    def __init(self):
+        Character__init__(name,hp,atk,defence,coin)
+        
+player = Character('Ryan',100,10,4,0,0)
+monster = Monster('monster',10,1,1,10,10)
+        
+
+    
