d02dafda3f79b50e8e9918d5c750aac3081d2753
ryanlid87 <rlidster1@gmail.com>
2016-01-08 15:35:05 -0600

    Update rpg.py
    Updated to auto make character, random battles with monsters that regenerate, new commands, and only have auto battle

--- rpg.py
+++ rpg.py
@@ -8,6 +8,8 @@ class Character(object):
         self.defence = defence
         self.exp = exp
         self.coin = coin
+        self.x = 0
+        self.y = 0
         
     def attack(self,monster):
         if self.atk < monster.defence:
@@ -18,6 +20,7 @@ class Character(object):
             self.exp = self.exp + monster.exp
             self.coin = self.coin + monster.coin
             return '%s died and gave %s exp and %s coins.' %(monster.name,monster.exp,monster.coin)
+
         return '%s was attacked for %s HP = %s' %(monster.name,(self.atk-monster.defence),monster.hp)
 
     def run(self,monster):
@@ -26,14 +29,79 @@ class Character(object):
             return '%s ran sucessfully.' %(self.name)
         else:
             return '%s was too fast %s could not get away.' %(monster.name,self.name)
-        
+
+    def walkR(self):
+        self.x = self.x + 1
+        area = randint(0,1)
+        if  area == 1:
+            self.monster = Monster()
+            self.battle(self.monster)
+        return 'coord(%s,%s)' %(self.x,self.y)
+    def walkL(self):
+        self.x = self.x - 1
+        area = randint(0,1)
+        if  area == 1:
+            self.monster = Monster()
+            self.battle(self.monster)
+        return 'coord(%s,%s)' %(self.x,self.y)
+    def walkU(self):
+        self.y = self.y + 1
+        area = randint(0,1)
+        if  area == 1:
+            self.monster = Monster()
+            self.battle(self.monster)
+        return 'coord(%s,%s)' %(self.x,self.y)
+    def walkD(self):
+        self.y = self.y - 1
+        area = randint(0,1)
+        if  area == 1:
+            self.monster = Monster()
+            self.battle(self.monster)
+        return 'coord(%s,%s)' %(self.x,self.y)
+    def battle(self,monster):
+        while monster.hp > 1:
+            print self.attack(monster)
+            print monster.attack(player)
+        self.monster = None
+    def status(self):
+        return 'hp = %s, atk = %s, def = %s, exp = %s, coins = %s' %(player.hp,player.atk,player.defence,player.exp,player.coin)
+    def help(self):
+        return ''' Here are the controls for the game:
+        up - moves character up one pixel
+        down - moves character down one pixel
+        left - moves character left one pixel
+        right - moves character right one pixel
+        status - shows status of character
+                '''
+
+                
 class Monster(Character):
     def __init__(self):
-        Character.__init__(self,'Monster',10,2,2,0,0)
-        self.key = 'penis'
+        Character.__init__(self,'Monster',10,2,2,1,1)
+        #equip has name of weapon, atk value
+        self.equip = ['Rubber Sword',1]
+        self.atk = self.atk + self.equip[1]
         
 player = Character('Ryan',100,10,4,0,0)
-monster = Monster()
-        
 
-    
+Commands = {
+    'up': player.walkU,
+    'down': player.walkD,
+    'left': player.walkL,
+    'right': player.walkR,
+    'status': player.status,
+    'help': player.help,
+    }
+
+while(player.hp > 0):
+    line = raw_input("> ")
+    args = line.split()
+    if len(args) > 0:
+        commandFound = False
+        for c in Commands.keys():
+            if args[0] == c[:len(args[0])]:
+                print Commands[c]()
+                commandFound = True
+                break
+        if not commandFound:
+            print "%s doesn't understand the suggestion." % player.name
